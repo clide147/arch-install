2f8fc3c3300a06673df5974533c3f15e545df859
Mitchell <mitchell.bosman@selu.edu>
2020-04-01 01:16:51 -0500

    stupid archroot

--- insideroot.sh
+++ insideroot.sh
@@ -1,34 +1,3 @@
-# set time and date
-timedatectl set-ntp true
-
-# Make partitions.
-echo "Select disk (sda or sdb):"
-read disk
-sgdisk --zap-all /dev/$disk                     # Erase everything
-sgdisk -n 0:0:+10MiB -t 0:ef02 /dev/$disk       # Bios Boot
-sgdisk -n 0:0:+500Mib -t 0:ef00 /dev/$disk      # EFI partition
-sgdisk -n 0:0:0 -t 0:8300 /dev/$disk            # Linux
-sgdisk -p /dev/$disk                            # List partitions, just in case.
-
-# Format drives.
-mkfs.fat -F32 /dev/${disk}2
-mkfs.ext4 /dev/${disk}3
-
-# Mount.
-mkdir -p /mnt/usb
-mount /dev/${disk}3 /mnt/usb
-mkdir /mnt/usb/boot
-mount /dev/${disk}2 /mnt/usb/boot
-
-# Install base system.
-pacstrap /mnt/usb linux linux-firmware base base-devel nano pantheon-terminal xorg-server firefox lightdm cinnamon grub efibootmgr networkmanager code git openssh
-
-# Use labels instead of UUIDs
-genfstab -U /mnt/usb > /mnt/usb/etc/fstab
-
-# enter system as root
-arch-chroot /mnt/usb
-
 # Setup locale and hostname
 ln -sf /usr/share/zoneinfo/America/Chicago /etc/localtime
 hwclock -systohc
@@ -80,5 +49,3 @@ echo "%wheel ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 
 # Finish up and Reboot:
 exit
-umount /mnt/usb/boot /mnt/usb && sync
-reboot now
