5d2f2db0902f03b0fcdcfc432855097d7d25fc08
Mitchell <mitchell.bosman@selu.edu>
2020-03-31 23:52:41 -0500

    I know how to script....

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 echo "Select disk (sda or sdb):"
 read disk
+echo $disk
 
-sgdisk --zap-all /dev/$(disk)
-sgdisk -n 0:0:+10MiB -t 0:ef02 /dev/$(disk)
-sgdisk -n 0:0:+500Mib -t 0:ef00 /dev/$(disk)
-sgdisk -n 0:0:0 -t 0:8300 /dev/$(disk)
-sgdisk -p /dev/$(disk)
+sgdisk --zap-all /dev/$disk
+sgdisk -n 0:0:+10MiB -t 0:ef02 /dev/$disk
+sgdisk -n 0:0:+500Mib -t 0:ef00 /dev/$disk
+sgdisk -n 0:0:0 -t 0:8300 /dev/$disk
+sgdisk -p /dev/$disk
