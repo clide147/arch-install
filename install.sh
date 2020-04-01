a6a106723bab4a57c92bbe03ce3407fac8b3afa2
Mitchell <mitchell.bosman@selu.edu>
2020-03-31 23:48:13 -0500

    start of formatting

---
+++ install.sh
@@ -0,0 +1,8 @@
+echo "Select disk (sda or sdb):"
+read disk
+
+sgdisk --zap-all /dev/$(disk)
+sgdisk -n 0:0:+10MiB -t 0:ef02 /dev/$(disk)
+sgdisk -n 0:0:+500Mib -t 0:ef00 /dev/$(disk)
+sgdisk -n 0:0:0 -t 0:8300 /dev/$(disk)
+sgdisk -p /dev/$(disk)
