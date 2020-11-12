5a8b61f1a4d19e66f8c2b9ddcc2f6e35d5f8eabb
Mitchell Bosman <mitchell.m.bosman@gmail.com>
2020-11-12 14:31:20 -0600

    uefi changes

---
+++ uefi-insideroot.sh
@@ -0,0 +1,119 @@
+echo "What was the drive path again? (/dev/sda)"
+read DISK
+bootctl install
+cat <<EOF > /boot/loader/entries/arch.conf
+title Arch Linux  
+linux /vmlinuz-linux  
+initrd  /initramfs-linux.img  
+options root=${DISK}1 rw
+EOF
+
+echo "--------------------------------------"
+echo "--          Network Setup           --"
+echo "--------------------------------------"
+pacman -S networkmanager dhclient --noconfirm --needed
+systemctl enable --now NetworkManager
+
+echo "--------------------------------------"
+echo "--      Set Password for Root       --"
+echo "--------------------------------------"
+echo "Enter password for root user: "
+passwd root
+
+exit
+
+# ln -sf /usr/share/zoneinfo/America/Chicago /etc/localtime
+# hwclock --systohc
+# echo en_US.UTF-8 UTF-8 > /etc/locale.gen
+# locale-gen
+# echo LANG=en_US.UTF-8 > /etc/locale.conf
+# echo "What hostname do you want?"
+# read hostname
+# echo $hostname > /etc/hostname
+# echo "127.0.0.1     localhost" > /etc/hosts
+# echo "::1     localhost" >> /etc/hosts
+# echo "127.0.1.1     $hostname.localdomain   $hostname" >> /etc/hosts
+# echo "--------------------------------------------------------"
+# #setup Ramdisk:
+# sed 's/HOOKS/#HOOKS/g' /etc/mkinitcpio.conf
+# echo "HOOKS=(base udev block filesystems keyboard fsck)" > /etc/mkinitcpio.conf
+# mkinitcpio -p linux
+# echo "--------------------------------------------------------"
+
+# # Setup network interface names to wlan0 and eth0 as default
+# ln -s /dev/null /etc/udev/rules.d/80-net-setup-link.rules
+
+# # Limit Journaling:
+# echo "Storage=volatile" >> /etc/systemd/journald.conf
+# echo "SystemMaxUse=16M" >> /etc/systemd/journald.conf
+# sed 's/relatime/noatime/g' /etc/fstab
+# echo "--------------------------------------------------------"
+
+# # This might help things:
+# rm -f /var/lib/pacman/db.lck
+
+# echo -e "\nInstalling Base System\n"
+
+# PKGS=(
+
+#     # --- XORG Display Rendering
+#         'xorg'                  # Base Package
+#         'xorg-drivers'          # Display Drivers 
+#         'xterm'                 # Terminal for TTY
+#         'xorg-server'           # XOrg server
+#         'xorg-apps'             # XOrg apps group
+#         'xorg-xinit'            # XOrg init
+#         'xorg-xinput'           # Xorg xinput
+#         'mesa'                  # Open source version of OpenGL
+
+#     # --- Setup Desktop
+#         'awesome'               # Awesome Desktop
+#         'xfce4-power-manager'   # Power Manager 
+#         'rofi'                  # Menu System
+#         'picom'                 # Translucent Windows
+#         'xclip'                 # System Clipboard
+#         'gnome-polkit'          # Elevate Applications
+#         'lxappearance'          # Set System Themes
+
+#     # --- Login Display Manager
+#         'lightdm'                   # Base Login Manager
+#         'lightdm-webkit2-greeter'   # Framework for Awesome Login Themes
+
+#     # --- Networking Setup
+#         'wpa_supplicant'            # Key negotiation for WPA wireless networks
+#         'dialog'                    # Enables shell scripts to trigger dialog boxex
+#         'openvpn'                   # Open VPN support
+#         'networkmanager-openvpn'    # Open VPN plugin for NM
+#         'network-manager-applet'    # System tray icon/utility for network connectivity
+#         'libsecret'                 # Library for storing passwords
+    
+#     # --- Audio
+#         'alsa-utils'        # Advanced Linux Sound Architecture (ALSA) Components https://alsa.opensrc.org/
+#         'alsa-plugins'      # ALSA plugins
+#         'pulseaudio'        # Pulse Audio sound components
+#         'pulseaudio-alsa'   # ALSA configuration for pulse audio
+#         'pavucontrol'       # Pulse Audio volume control
+#         'pnmixer'           # System tray volume control
+
+#     # --- Bluetooth
+#         'bluez'                 # Daemons for the bluetooth protocol stack
+#         'bluez-utils'           # Bluetooth development and debugging utilities
+#         'bluez-firmware'        # Firmwares for Broadcom BCM203x and STLC2300 Bluetooth chips
+#         'blueberry'             # Bluetooth configuration tool
+#         'pulseaudio-bluetooth'  # Bluetooth support for PulseAudio
+    
+#     # --- Printers
+#         'cups'                  # Open source printer drivers
+#         'cups-pdf'              # PDF support for cups
+#         'ghostscript'           # PostScript interpreter
+#         'gsfonts'               # Adobe Postscript replacement fonts
+#         'hplip'                 # HP Drivers
+#         'system-config-printer' # Printer setup  utility
+# )
+
+# for PKG in "${PKGS[@]}"; do
+#     echo "INSTALLING: ${PKG}"
+#     sudo pacman -S "$PKG" --noconfirm --needed
+# done
+
+# echo -e "\nDone!\n"
---
+++ uefi-install.sh
@@ -0,0 +1,41 @@
+# set time and date
+timedatectl set-ntp true
+# Make partitions.
+lsblk
+echo "Select disk (sda or sdb):"
+read disk
+sgdisk --zap-all /dev/$disk                     # Erase everything
+sgdisk -n 0:0:+500Mib -t 0:ef00 /dev/$disk      # EFI partition
+sgdisk -n 0:0:0 -t 0:8300 /dev/$disk            # Linux
+sgdisk -p /dev/$disk                            # List partitions, just in case.
+
+# Format drives.
+mkfs.fat -F32 /dev/${disk}1
+mkfs.ext4 -F /dev/${disk}2
+
+# Mount.
+mkdir -p /mnt
+mount /dev/${disk}2 /mnt
+mkdir /mnt/boot
+mount /dev/${disk}1 /mnt/boot
+
+# Update package database
+pacman -Sy --noconfirm reflector
+reflector --verbose --country 'United States' -l 15 --sort rate --save /etc/pacman.d/mirrorlist
+
+# Install base system.
+pacstrap /mnt linux linux-firmware base base-devel nano sudo --noconfirm --needed
+
+# Use labels instead of UUIDs
+genfstab -U /mnt > /mnt/etc/fstab
+
+# Copy post-install to root directory
+cp -rfv uefi-insideroot.sh /mnt/root
+cp -rfv installsoftwares.sh /mnt/root
+chmod +x /mnt/root/uefi-insideroot.sh
+# enter system as root
+arch-chroot /mnt ~/./uefi-insideroot.sh
+
+# Finished running.... then unmount and reboot
+umount /mnt/boot /mnt && sync
+reboot now
